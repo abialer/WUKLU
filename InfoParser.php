f90b799f1beebd0a1430858b8db208efdef7a1a6
John Garza <johnegarza@wustl.edu>
2016-10-22 17:08:28 -0500

    synchronizing code between isaac and john's work

--- InfoParser.php
+++ InfoParser.php
@@ -1,6 +1,6 @@
 <?php
 
-    require 'dbconnect.php';
+    require 'database.php';
 
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$mysqli = new mysqli('localhost', 'GH', 'GH', 'GH');
-
-	if($mysqli -> connect_errno){
-        printf("Connection Failed: %s\n", $mysqli -> connect_errno);
-        exit;
-    }
-?>
