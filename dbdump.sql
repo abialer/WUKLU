f3deec0e7697f99fb341b8214659fff7153d1c69
johnegarza <johnegarza@gmail.com>
2016-10-22 15:31:52 -0500

    Create dbdump.sql

---
+++ dbdump.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `MainInfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `number` tinyint(10) NOT NULL,
+  `gender` enum('MALE','FEMALE','OTHER') NOT NULL,
+  `veteran` enum('TRUE','FALSE') NOT NULL,
+  `mental` enum('TRUE','FALSE') NOT NULL,
+  `dob` date NOT NULL,
+  `enthnicity` enum('Hispanic/Latino','Not Hispanic/Latino','Prefer not to respond') NOT NULL,
+  `race` enum('American Indian/Alaska Native','Asian','Black or African American','Native Hawaiian/Pacific Islander','White') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
